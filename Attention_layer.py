b09d0cb0573fa399d9f462cc1122c709160ded43
JinSeoung-Oh <78573459+JinSeoung-Oh@users.noreply.github.com>
2023-03-13 19:46:44 +0900

    Create Attention_layer.py

---
+++ Attention_layer.py
@@ -0,0 +1,25 @@
+from tensorflow.keras.layers import Layer
+import tensorflow.keras.backend as K
+
+
+class attention_1(Layer):
+    def __init__(self,**kwargs):
+        super(attention_1,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")        
+        super(attention_1, self).build(input_shape)
+
+    def call(self,x):
+        et=K.squeeze(K.tanh(K.dot(x,self.W)+self.b),axis=-1)
+        at=K.softmax(et)
+        at=K.expand_dims(at,axis=-1)
+        output=x*at
+        return K.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention_1,self).get_config()
